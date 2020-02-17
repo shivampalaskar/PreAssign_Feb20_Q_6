d6a6f5b86124cbbe7f57d3749a895e86ceed04d2
Shivam Palaskar <59534343+shivampalaskar@users.noreply.github.com>
2020-02-17 19:00:40 +0530

    Update PreAssign_Feb20_Q_6.c

--- PreAssign_Feb20_Q_6/src/PreAssign_Feb20_Q_6.c
+++ PreAssign_Feb20_Q_6/src/PreAssign_Feb20_Q_6.c
@@ -10,12 +10,16 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include<string.h>
 
 #define SIZE 5
 void enqueue();
 void dequeue();
 int isFull();
 int isEmpty();
+void display();
+void printData();
+void getData();
 
 struct Student {
 	int rollNo;
@@ -29,18 +33,24 @@ char subject[6][20] = { "Physics", "Chemistry", "Mathematics", "Environment",
 struct Student student[SIZE];
 int rear = -1, front = -1;
 
+int menuList(){
+	int choice;
+	printf("\n\nChoose operation to perform: ");
+	printf("\n1) Add a Student");
+	printf("\n2) Remove a Student");
+	printf("\n3) Display");
+	printf("\n4) Exit");
+	printf("\n Enter a Choice : ");
+	scanf("%d", &choice);
+	return choice;
+}
+
 int main(void) {
 	setvbuf(stdout,NULL,_IONBF,0);
 
 	int choice;
 	do {
-		printf("\n\nChoose operation to perform: ");
-		printf("\n1) Add a Student");
-		printf("\n2) Remove a Student");
-		printf("\n3) Display");
-		printf("\n4) Exit");
-		printf("\n Enter a Choice : ");
-		scanf("%d", &choice);
+		choice = menuList();
 
 		switch (choice) {
 		case 1:
@@ -78,18 +88,22 @@ int isFull() {
 	else
 		return 0;
 }
-void enqueue() {
-	rear = (rear + 1) % SIZE;
+
+void getData() {
 	printf("\nEnter Roll No : ");
-	scanf("%d",&student[rear].rollNo);
+	scanf("%d", &student[rear].rollNo);
 	printf("\nEnter Name of Student : ");
-	scanf("%s",student[rear].name);
+	scanf("%s", student[rear].name);
 	fflush(stdin);
 	printf("\n Enter Standard of Student : ");
-	scanf("%d",&student[rear].std);
+	scanf("%d", &student[rear].std);
 	printf("\n Enter marks obtained by student : ");
 	printf("\n Subject   Marks");
+}
 
+void enqueue() {
+	rear = (rear + 1) % SIZE;
+	getData();
 	for(int i=0;i<6;i++){
 		strcpy(student[rear].sub[i],subject[i]);
 		printf("\n%d) %s ",i+1,subject[i]);
@@ -111,22 +125,20 @@ void dequeue() {
 void display() {
 	int r = rear, f = front;
 	while (f != r) {
-			printf("\n\n Student roll No : %d",student[f].rollNo);
-			printf("\n Student Name : %s",student[f].name);
-			printf("\n Student Standard : %d",student[f].std);
-			printf("\n Student Subjects and Marks : ");
-			printf("\n Subject   Marks");
-			for(int i=0;i<6;i++){
-				printf("\n%d) %s %d",i+1,student[f].sub[i],student[f].mark[i]);
-		}
+		printData();
 		f = (f + 1) % SIZE;
 	}
+	printData();
+}
+
+void printData() {
+	int f = front;
 	printf("\n\n Student roll No : %d", student[f].rollNo);
 	printf("\n Student Name : %s", student[f].name);
 	printf("\n Student Standard : %d", student[f].std);
 	printf("\n Student Subjects and Marks : ");
 	printf("\n Subject   Marks");
 	for (int i = 0; i < 6; i++) {
-		printf("\n%d) %s %d", i + 1, student[f].sub[i], student[f].mark[i]);
+		printf("\n%d) %-s %d", i + 1, student[f].sub[i], student[f].mark[i]);
 	}
 }
